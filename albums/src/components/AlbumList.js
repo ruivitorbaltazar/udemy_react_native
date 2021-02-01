682dbce2962d354b95b1c26a7e8a40cbfb04024b
ruivitorbaltazar <ruibaltazar@Ruis-iMac.lan>
2021-02-01 13:33:51 +0000

    finished album app; connected to discogs api; up to 316

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import cheerio from 'cheerio';
 import AlbumDetail from './AlbumDetail';
@@ -7,66 +7,78 @@ import AlbumDetail from './AlbumDetail';
 class AlbumList extends Component {
 	state = { albums: [] };
 
-	// addCoverImages(allAlbums) {
-		
-	// 	// const allAlbums = this.state.albums;
-		
-	// 	// fetching resource url from release url
-	// 	const fetchResourceUrl = async (album) => {
-	// 		try {
-	// 			await axios.get(album.resource_url)
-	// 			.then(response => {
-	// 				fetchImageUrl(album, response.data)
-	// 			})
-	// 		} catch (error) {
-	// 			console.log(error.message)
-	// 		}
-	// 	}
-		
-	// 	// fetching image url from resource url
-	// 	const fetchImageUrl = async (album, resource) => {
-	// 		try {
-	// 			await axios.get(resource.uri)
-	// 			.then(response => {
-	// 				const $ = cheerio.load(response.data);
-	// 				const imageUrl = $('.thumbnail_center > img')[0]
-	// 				? $('.thumbnail_center > img')[0].attribs.src
-	// 				: 'https://community.mp3tag.de/uploads/default/original/2X/a/acf3edeb055e7b77114f9e393d1edeeda37e50c9.png';
-	// 				const newAlbums = [...this.state.albums].map(a => a === album ? a.image_url = imageUrl : a)
-	// 				console.log(this.state.albums);
-	// 				this.setState({ albums: newAlbums })
-	// 			})
-	// 		} catch (error) {
-	// 			console.log(error.message)
-	// 		}
-	// 	}
-		
-	// 	// set images in albums
-	// 	return allAlbums.map(album => {
-	// 		album = fetchResourceUrl(album);
-	// 		return album
-	// 	});
-	// };
-	
-	// componentWillMount() {			SOON TO BE DEPRECATED
-	
 	componentDidMount() {
-		// Album API is down. Using discogs api.
-		axios.get('https://api.discogs.com/artists/3840/releases?')
-		.then(response => {
-			this.setState({ albums: response.data.releases.slice(0,5) });
-		});
+		// componentWillMount() {			SOON TO BE DEPRECATED
+		// Exercise album API is down. Using discogs api.
+		
+		const discogsArtist = {
+			name: 'Radiohead',
+			number: '3840'
+		};
+		// scrape artist page for artist image source
+		let artistImageUrl;
+
+		try {
+			axios.get(`https://www.discogs.com/artist/${discogsArtist.number}-${discogsArtist.name}`)
+			.then(response => {
+				const $ = cheerio.load(response.data);
+				artistImageUrl = $('.thumbnail_center > img')[0]
+				? $('.thumbnail_center > img')[0].attribs.src
+				: 'https://community.mp3tag.de/uploads/default/original/2X/a/acf3edeb055e7b77114f9e393d1edeeda37e50c9.png';
+			});
+		} catch(error) {
+			console.log(error.message)
+		}
+		
+		try {
+			axios.get(`https://api.discogs.com/artists/${discogsArtist.number}/releases?sort=year`)
+			.then(response => {
+				const apiAlbums = response.data.releases.slice(0,5);
+				
+				apiAlbums.map(album => {
+					// fetch album url
+					try {
+						axios.get(album.resource_url)
+						.then(response => {
+							// scrape album page for album image source and marketplace
+							try {
+								axios.get(response.data.uri)
+								.then(response => {
+									const $ = cheerio.load(response.data);
+									const albumImageUrl = $('.thumbnail_center > img')[0]
+									? $('.thumbnail_center > img')[0].attribs.src
+									: 'https://community.mp3tag.de/uploads/default/original/2X/a/acf3edeb055e7b77114f9e393d1edeeda37e50c9.png';
+									const marketplaceUrl = $('.marketplace_box_links .section_content > a')[0].attribs.href
+
+									album.artist_image_url = artistImageUrl;
+									album.album_image_url = albumImageUrl;
+									album.marketplace_url = `https://www.discogs.com${marketplaceUrl}`;
+								})
+							} catch(error) {
+								console.log(error.message)
+							}
+						});
+					} catch(error) {
+						console.log(error.message)
+					}
+				});			
+				
+				this.setState({ albums: apiAlbums });
+			});
+		} catch(error) {
+			console.log(error.message)
+		}
 	}
 	
 	renderAlbums() {
-		return this.state.albums.map(album => <AlbumDetail key={album.id} album={ album } />)  
+		return this.state.albums.map(album => <AlbumDetail key={ album.id } album={ album } />)  
 	}
 
 	render() {
 		return (
-			<View>
+			<ScrollView>
 				{ this.renderAlbums() }
-			</View>
+			</ScrollView>
 		)
 	}
 }
